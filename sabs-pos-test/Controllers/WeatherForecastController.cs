a14b0ccdc48a8a1f0204209c59f2fd832b2072df
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-15 23:48:30 +0700

    zz

--- sabs-pos-test/Controllers/WeatherForecastController.cs
+++ sabs-pos-test/Controllers/WeatherForecastController.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace sabs_pos_test.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("v{version:apiVersion}/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
