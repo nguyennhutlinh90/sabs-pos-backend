cba514b0aa95ef42fda47072f02846bc69086794
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-15 22:57:15 +0700

    Remove serilog

--- sabs-pos-test/Controllers/WeatherForecastController.cs
+++ sabs-pos-test/Controllers/WeatherForecastController.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace sabs_pos_test.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- sabs-pos-test/Program.cs
+++ sabs-pos-test/Program.cs
@@ -2,8 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
-using Serilog;
-
 using System;
 using System.IO;
 
@@ -13,25 +11,7 @@ namespace sabs_pos_test
     {
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                            .MinimumLevel.Information()
-                            .WriteTo.File($"{AppDomain.CurrentDomain.BaseDirectory}/logs/log-.txt", rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
-                            .CreateLogger();
-
-            try
-            {
-                Log.Information($"Application start-up on {DateTime.Now:yyy-MM-dd HH:mm:ss.fff}");
-
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -52,7 +32,6 @@ namespace sabs_pos_test
                     webHostBuilder.UseSetting(WebHostDefaults.DetailedErrorsKey, "true");
                     webHostBuilder.UseStartup<Startup>();
                 })
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseSerilog();
+                .UseContentRoot(Directory.GetCurrentDirectory());
     }
 }
