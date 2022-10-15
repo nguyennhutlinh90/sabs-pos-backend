9a9d797c41a6e4a35e3eb5806f77384e2794e6ef
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-15 23:11:43 +0700

    tt

--- sabs-pos-test/Program.cs
+++ sabs-pos-test/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
 using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace sabs_pos_test
 {
@@ -16,22 +18,9 @@ namespace sabs_pos_test
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, builder) =>
+                .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-                    builder.SetBasePath(Directory.GetCurrentDirectory());
-                    builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-                    if (!string.IsNullOrEmpty(env))
-                        builder.AddJsonFile($"appsettings.{env}.json", optional: true);
-                    builder.AddEnvironmentVariables();
-                })
-                .ConfigureWebHostDefaults(webHostBuilder =>
-                {
-                    webHostBuilder.UseKestrel();
-                    webHostBuilder.UseIISIntegration();
-                    webHostBuilder.UseSetting(WebHostDefaults.DetailedErrorsKey, "true");
-                    webHostBuilder.UseStartup<Startup>();
-                })
-                .UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
