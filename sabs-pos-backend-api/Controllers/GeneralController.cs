3e51f4ea6540d5c07ff65625aaace7dcc0a8860c
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-16 18:38:16 +0700

    dasa

--- sabs-pos-backend-api/Controllers/GeneralController.cs
+++ sabs-pos-backend-api/Controllers/GeneralController.cs
@@ -22,17 +22,17 @@ namespace sabs_pos_backend_api.Controllers
         {
             return Execute(() =>
             {
-                var appSettings = new AppSettings();
-                appSettings.Configuration.ConnectionString = AesCrypto.Decrypt(_appSettings.Configuration.ConnectionString);
-                appSettings.SeedData.AccountName = AesCrypto.Decrypt(_appSettings.SeedData.AccountName);
-                appSettings.SeedData.AccountEmail = AesCrypto.Decrypt(_appSettings.SeedData.AccountEmail);
-                appSettings.SeedData.AccountPass = AesCrypto.Decrypt(_appSettings.SeedData.AccountPass);
-                appSettings.LinkSettings.EmailConfirmation = AesCrypto.Decrypt(_appSettings.LinkSettings.EmailConfirmation);
-                appSettings.LinkSettings.ResetPassword = AesCrypto.Decrypt(_appSettings.LinkSettings.ResetPassword);
-                appSettings.MailSettings.Sender = AesCrypto.Decrypt(_appSettings.MailSettings.Sender);
-                appSettings.MailSettings.SmtpHost = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpHost);
-                appSettings.MailSettings.SmtpUser = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpUser);
-                appSettings.MailSettings.SmtpPass = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpPass);
+                var appSettings = _appSettings;
+                //appSettings.Configuration.ConnectionString = AesCrypto.Decrypt(_appSettings.Configuration.ConnectionString);
+                //appSettings.SeedData.AccountName = AesCrypto.Decrypt(_appSettings.SeedData.AccountName);
+                //appSettings.SeedData.AccountEmail = AesCrypto.Decrypt(_appSettings.SeedData.AccountEmail);
+                //appSettings.SeedData.AccountPass = AesCrypto.Decrypt(_appSettings.SeedData.AccountPass);
+                //appSettings.LinkSettings.EmailConfirmation = AesCrypto.Decrypt(_appSettings.LinkSettings.EmailConfirmation);
+                //appSettings.LinkSettings.ResetPassword = AesCrypto.Decrypt(_appSettings.LinkSettings.ResetPassword);
+                //appSettings.MailSettings.Sender = AesCrypto.Decrypt(_appSettings.MailSettings.Sender);
+                //appSettings.MailSettings.SmtpHost = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpHost);
+                //appSettings.MailSettings.SmtpUser = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpUser);
+                //appSettings.MailSettings.SmtpPass = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpPass);
                 return appSettings;
             });
         }
