d935a4661a2394ecdd6c46bbf007479c3589905b
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-16 16:30:36 +0700

    jjjj

--- sabs-pos-backend-api/Startup.cs
+++ sabs-pos-backend-api/Startup.cs
@@ -1,3 +1,4 @@
+using CoreLib.Crypto;
 using CoreLib.PasswordHasher;
 using CoreLib.SqlExtensions;
 
@@ -78,6 +79,16 @@ namespace sabs_pos_backend_api
                 .SetCompatibilityVersion(CompatibilityVersion.Latest);
 
             var appSettings = Configuration.Get<AppSettings>();
+            appSettings.Configuration.ConnectionString = AesCrypto.Decrypt(appSettings.Configuration.ConnectionString);
+            appSettings.SeedData.AccountName = AesCrypto.Decrypt(appSettings.SeedData.AccountName);
+            appSettings.SeedData.AccountEmail = AesCrypto.Decrypt(appSettings.SeedData.AccountEmail);
+            appSettings.SeedData.AccountPass = AesCrypto.Decrypt(appSettings.SeedData.AccountPass);
+            appSettings.LinkSettings.EmailConfirmation = AesCrypto.Decrypt(appSettings.LinkSettings.EmailConfirmation);
+            appSettings.LinkSettings.ResetPassword = AesCrypto.Decrypt(appSettings.LinkSettings.ResetPassword);
+            appSettings.MailSettings.Sender = AesCrypto.Decrypt(appSettings.MailSettings.Sender);
+            appSettings.MailSettings.SmtpHost = AesCrypto.Decrypt(appSettings.MailSettings.SmtpHost);
+            appSettings.MailSettings.SmtpUser = AesCrypto.Decrypt(appSettings.MailSettings.SmtpUser);
+            appSettings.MailSettings.SmtpPass = AesCrypto.Decrypt(appSettings.MailSettings.SmtpPass);
 
             services.AddSingleton<IAppSettings>(appSettings);
             services.AddSingleton<IPasswordHasher, PasswordHasher>();
