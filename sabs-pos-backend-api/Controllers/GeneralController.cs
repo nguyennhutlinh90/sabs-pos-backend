d0c96f7508b5333362dad72475985709e0c94807
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-16 18:29:50 +0700

    sdfds

---
+++ sabs-pos-backend-api/Controllers/GeneralController.cs
@@ -0,0 +1,40 @@
+﻿using CoreLib.Crypto;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace sabs_pos_backend_api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}")]
+    public class GeneralController : ActionResultBase
+    {
+        readonly IAppSettings _appSettings;
+        public GeneralController(IAppSettings appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        [HttpGet]
+        [ApiVersionNeutral]
+        [Route("settings")]
+        public IActionResult Settings()
+        {
+            return Execute(() =>
+            {
+                var appSettings = new AppSettings();
+                appSettings.Configuration.ConnectionString = AesCrypto.Decrypt(_appSettings.Configuration.ConnectionString);
+                appSettings.SeedData.AccountName = AesCrypto.Decrypt(_appSettings.SeedData.AccountName);
+                appSettings.SeedData.AccountEmail = AesCrypto.Decrypt(_appSettings.SeedData.AccountEmail);
+                appSettings.SeedData.AccountPass = AesCrypto.Decrypt(_appSettings.SeedData.AccountPass);
+                appSettings.LinkSettings.EmailConfirmation = AesCrypto.Decrypt(_appSettings.LinkSettings.EmailConfirmation);
+                appSettings.LinkSettings.ResetPassword = AesCrypto.Decrypt(_appSettings.LinkSettings.ResetPassword);
+                appSettings.MailSettings.Sender = AesCrypto.Decrypt(_appSettings.MailSettings.Sender);
+                appSettings.MailSettings.SmtpHost = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpHost);
+                appSettings.MailSettings.SmtpUser = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpUser);
+                appSettings.MailSettings.SmtpPass = AesCrypto.Decrypt(_appSettings.MailSettings.SmtpPass);
+                return appSettings;
+            });
+        }
+    }
+}
