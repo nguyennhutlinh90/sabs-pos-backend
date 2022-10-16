42706d46b1a49a905128b16b461fa2e8b6f36168
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-16 16:08:34 +0700

    ccc

--- sabs-pos-backend-api/Startup.cs
+++ sabs-pos-backend-api/Startup.cs
@@ -1,3 +1,7 @@
+using CoreLib.PasswordHasher;
+using CoreLib.SqlExtensions;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -7,16 +11,20 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 
 using Newtonsoft.Json;
+
+using sabs_pos_backend_api.Models;
 
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace sabs_pos_backend_api
 {
@@ -68,6 +76,83 @@ namespace sabs_pos_backend_api
                     //options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
                 })
                 .SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+            var appSettings = Configuration.GetSettings();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(appSettings.Configuration.JwtTokenKey.ToBytes()),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+                x.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = async (context) =>
+                    {
+                        if (context.Principal != null)
+                        {
+                            //var uuid = context.Principal.FindFirstValue(ClaimType.LOGIN_UUID);
+                            //if (!string.IsNullOrEmpty(uuid))
+                            //{
+                            //    var srvAccount = context.HttpContext.RequestServices.GetService<IAccountService>();
+                            //    var account = await srvAccount.Get<AccountRes>(QF.EQ("uuid", uuid), include: QI.IC("roles", "uuid role_uuid"));
+                            //    if (!account.IsNull())
+                            //    {
+                            //        var password = context.Principal.FindFirstValue(ClaimType.PASSWORD);
+                            //        var role_uuid = context.Principal.FindFirstValue(ClaimType.ROLE_UUID);
+                            //        if (account.password_hash == password && account.role_uuid == role_uuid)
+                            //        {
+                            //            context.Success();
+                            //            return;
+                            //        }
+                            //    }
+                            //}
+
+                            context.Success();
+                            return;
+                        }
+                        context.Fail("Unauthorized");
+                    }
+                };
+            });
+
+            services.AddSqlHandler((configBuider) =>
+            {
+                configBuider.ConnectionString = appSettings.Configuration.ConnectionString;
+                configBuider.UpdateError = (error) =>
+                {
+                    error.Function.TableColumnsNotFound = (prefix, table, columns) =>
+                    {
+                        return string.Format("{0} field(s) ({1}) were not found", prefix, string.Join(", ", columns));
+                    };
+                    error.Function.TableColumnsRequired = (prefix, table) =>
+                    {
+                        return string.Format("{0} table '{1}' field(s) are required", prefix, table);
+                    };
+                    error.Prefix.Select = "Cursor";
+                };
+            });
+
+            services.AddSingleton<IAppSettings>(appSettings);
+            services.AddSingleton<IPasswordHasher, PasswordHasher>();
+
+            services.AddSingleton<IAccountService, AccountService>();
+            services.AddSingleton<IActivityLogService, ActivityLogService>();
+            services.AddSingleton<IEmployeeService, EmployeeService>();
+            services.AddSingleton<IMailService, MailService>();
+            services.AddSingleton<IPOSDeviceService, POSDeviceService>();
+            services.AddSingleton<IRoleService, RoleService>();
+            services.AddSingleton<IStoreService, StoreService>();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigureOptions>();
