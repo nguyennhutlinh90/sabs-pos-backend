6969bf501744d37989062082dd7a725859ec4a33
nguyennhutlinh90 <linh.nhut.nguyen@delfi.dk>
2022-10-16 20:32:38 +0700

    sdfdsf

--- sabs-pos-backend-api/Controllers/GeneralController.cs
+++ sabs-pos-backend-api/Controllers/GeneralController.cs
@@ -22,18 +22,7 @@ namespace sabs_pos_backend_api.Controllers
         {
             return Execute(() =>
             {
-                var appSettings = _appSettings;
-                appSettings.Configuration.ConnectionString = _appSettings.Configuration.ConnectionString.Decrypt();
-                appSettings.SeedData.AccountName = _appSettings.SeedData.AccountName.Decrypt();
-                appSettings.SeedData.AccountEmail = _appSettings.SeedData.AccountEmail.Decrypt();
-                appSettings.SeedData.AccountPass = _appSettings.SeedData.AccountPass.Decrypt();
-                appSettings.LinkSettings.ConfirmEmail1 = _appSettings.LinkSettings.ConfirmEmail1.Decrypt();
-                appSettings.LinkSettings.ResetPassword = _appSettings.LinkSettings.ResetPassword.Decrypt();
-                appSettings.MailSettings.Sender = _appSettings.MailSettings.Sender.Decrypt();
-                appSettings.MailSettings.SmtpHost = _appSettings.MailSettings.SmtpHost.Decrypt();
-                appSettings.MailSettings.SmtpUser = _appSettings.MailSettings.SmtpUser.Decrypt();
-                appSettings.MailSettings.SmtpPass = _appSettings.MailSettings.SmtpPass.Decrypt();
-                return appSettings;
+                return _appSettings.Configuration.ConnectionString.Decrypt();
             });
         }
     }
